f0a64b2f566445518757738e8ea08e4ab0538377
GAHYUN-P <gahyun94526@gmail.com>
2022-01-31 22:47:54 +0900

    useHistory 훅을 사용해서 이동하기

--- src/Dog.js
+++ src/Dog.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Dog = (props) => {
+    const history = useHistory();
     console.log(props);
     // 안에서 history 객체를 확인할 수 있다
 
     return (
         <div onClick={() => {
-            props.history.push("/")
+            history.push("/cat")
             // push는 페이지 이동하는 내장함수
         }}>강아지 화면이에요.</div>
     )
