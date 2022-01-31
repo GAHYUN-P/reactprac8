5e1865d9091f1a3bb6c219a5f44467813ca545fd
GAHYUN-P <gahyun94526@gmail.com>
2022-01-31 21:47:21 +0900

    Link 사용해서 페이지 이동하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Home from "./Home"
 import Cat from "./Cat"
 import Dog from "./Dog"
@@ -6,13 +6,18 @@ import Dog from "./Dog"
 function App() {
   return (
     <div className="App">
+      <div>
+        <Link to="/">Home으로 가기</Link>
+        <Link to="/cat">Cat으로 가기</Link>
+        <Link to="/dog">Dog으로 가기</Link>
+      </div>
       <Route path="/" exact>
         {/* exact 속성이 없으면 포함하면 다보여줌
         들어가게 되면 패스가 완전히 똑같으면 보여줌! */}
         <Home data = {"im data"}/>
         {/* 페이지에 데이터 주는 법 */}
       </Route>
-      <Route path="/cat/:cat_name" component={Cat}>
+      <Route path="/cat" component={Cat}>
         {/* 동적라우팅 /:변수명 */}
         {/* <Cat/> */}
       </Route>
